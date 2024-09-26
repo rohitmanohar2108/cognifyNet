0b3e9d80d3ef9f4215cd5d00cf5c5677ad0a4868
Rohit Manohar <manoharrohit372@gmail.com>
2024-09-27 00:41:57 +0530

    done

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -56,7 +56,8 @@ const Home = () => {
             <img
               src={post.image}
               alt="Post"
-              className="w-full h-[500px] object-cover"
+              className="w-full h-[400px] object-cover rounded-lg"
+              style={{ maxWidth: '90%', margin: '0 auto' }}
             />
             <div className="flex justify-between items-center p-4">
               <div className="flex space-x-8">
